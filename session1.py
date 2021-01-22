1d76ce47e49a953cb839f1012d425c3d2873e3b0
otterbonkai <55806414+otterbonkai@users.noreply.github.com>
2021-01-23 00:54:12 +0530

    Add files via upload

---
+++ session1.py
@@ -0,0 +1,12 @@
+def myfunc():
+    string='Cppsecrets'
+    n=len(string)
+    arr=[]
+    for i in range(n):
+        for j in range(i+1,n+1):
+            a=string[i:j]
+            arr.append(a)
+        print(arr)
+
+def my_func():
+    pass
---
+++ test_session1.py
@@ -0,0 +1,19 @@
+import pytest
+import inspect
+from test_utils import *
+import re
+
+import session1
+
+
+WIDTH = 10
+HEIGHT = 20
+def test_fourspace_equal():
+    assert fourspace(session1) == False, 'Not all spaces before lines are a multiple of 4!'
+
+def test_function_names():
+    assert function_name_had_cap_letter(session1) == False, "One of your function has a capitalized alphabet!"
+
+# def test_rectangle_repr():
+#    r = session1.Rectangle(WIDTH, HEIGHT)
+#    assert r.__repr__() == f'Rectangle({WIDTH}, {HEIGHT})', 'The representation of the Rectangle object does not meet expectations'
